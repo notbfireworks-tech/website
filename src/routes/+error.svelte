543dd325e067ce4ccb35ed9e7f785ab334affa7b
notbfireworks-tech <notbfireworks@gmail.com>
2026-03-23 13:00:31 -0400

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,7 +4,7 @@
 
 <Container>
 	<h1>404 error</h1>
-	<h2>What a dud!</h2>
+	<h2>What a dud!!</h2>
 	<p>
 		This page is a dud. Fortunately, our fireworks work better than our website does (apparently).
 		Just head to our <a href="/">Home Page</a> and navigate around to find what you are looking for.
